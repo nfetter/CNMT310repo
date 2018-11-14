8bb44a29bbbec4a21f133032f8bbd1782c683bb1
alacy104 <alacy104@uwsp.edu>
2018-11-14 10:45:01 -0600

    .

--- underconstruction.php
+++ underconstruction.php
@@ -18,8 +18,8 @@ $page->setHeadSection("
     <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' integrity='sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49' crossorigin='anonymous'></script>
     <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js' integrity='sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy' crossorigin='anonymous'></script>
 
+	<link rel='stylesheet' href='css/main.css'>
 	 <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' integrity='sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO' crossorigin='anonymous'>
-<link rel='stylesheet' href='css/main.css'>
 <link rel='stylesheet' href='css/bootstrap-grid.css'>
         <link rel='stylesheet' href='css/bootstrap-reboot.css'>
         <link rel='stylesheet' href='css/bootstrap.css'>
@@ -33,8 +33,7 @@ print $page->getTopSection();
 
 /*for nav and bottom*/print $myPage->getTopSection();
 
-print "<img src='Pictures/underConstruction' class='shop' alt='Under Construction'>";
-
+print "<img src='Pictures/underConstruction' class='img-fluid center-block '  alt='Under Construction'>";
 /*for nav and bottom*/print $myPage->getBottomSection();
 
 
